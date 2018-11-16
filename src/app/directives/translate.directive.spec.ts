a9f26fe6dd0b99e43c8539735f64f43906dc63a1
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-11-16 13:27:32 +0700

    fix directive test

--- src/app/directives/translate.directive.spec.ts
+++ src/app/directives/translate.directive.spec.ts
@@ -1,16 +1,31 @@
 import { TranslateDirective } from './translate.directive';
 import { ElementRef } from '@angular/core';
 import { TranslationService } from '../services/translation.service';
+import { ITranslationService } from '../services/translation.service.interface';
+import { Subject } from 'rxjs';
 
 describe('TranslateDirective', () => {
   it('should create an instance', () => {
+    const MockTranslationService = jest.fn<ITranslationService​>(() => ({
+      translate: jest.fn().mockImplementation(() => {
+        return 'okok';
+      }),
+      translationChanged: jest.fn().mockImplementation(() => {
+        return (new Subject<void>()).asObservable();
+      })
+    }));
+    const translator = new MockTranslationService();
     const el: ElementRef = {
       nativeElement: {
-        innerText: ""
+        innerText: "test"
       }
     };
-    const translator: TranslationService = new TranslationService();
     const directive = new TranslateDirective(el, translator);
-    expect(directive).toBeTruthy();
+
+    directive.ngOnInit();
+
+    expect(translator.translate).toHaveBeenCalledWith('test');
+    expect(translator.translate).toHaveBeenCalledTimes(1);
+    expect(el.nativeElement.innerText).toBe('okok');
   });
 });
--- src/app/services/translation.service.interface.ts
+++ src/app/services/translation.service.interface.ts
@@ -1,4 +1,7 @@
+import { Observable } from "rxjs";
+
 export interface ITranslationService {
   setLanguage(string)
   translate(string): string
+  translationChanged(): Observable<void>
 }
